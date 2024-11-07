23cb03815eda4693703fa22120d32be6a584afe2
pack0shades <kumarpragay@gmail.com>
2024-11-07 22:21:15 +0530

    added pinecone db

--- args.py
+++ args.py
@@ -4,6 +4,7 @@ def get_args():
     arg = argparse.ArgumentParser()
     arg.add_argument("--use_reranker", type=bool, default=True, help="Use reranker or not")
     arg.add_argument("--retrieved_docs", type=int, default=5, help="Number of retrieved documents")
+    arg.add_argument("--collection_name", type=str, default="generate", help="Name of the collection")
     return  arg.parse_args()
 
 if __name__ == "__main__":
