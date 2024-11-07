077cfa62691ba7069d0708989705a8e53e3dd850
pack0shades <kumarpragay@gmail.com>
2024-11-08 00:11:48 +0530

    updated argparse

--- args.py
+++ args.py
@@ -5,6 +5,7 @@ def get_args():
     arg.add_argument("--use_reranker", type=bool, default=True, help="Use reranker or not")
     arg.add_argument("--retrieved_docs", type=int, default=5, help="Number of retrieved documents")
     arg.add_argument("--collection_name", type=str, default="generate", help="Name of the collection")
+    arg.add_argument("--pdf_path", type=str, default="./pdfs/nvidia.pdf", help="Path to the PDF document")
     return  arg.parse_args()
 
 if __name__ == "__main__":
