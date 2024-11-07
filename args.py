89467a8d50078b08c4b9fb4e063005e56b7ec36c
pack0shades <kumarpragay@gmail.com>
2024-11-07 11:28:20 +0530

    resolved issue

--- args.py
+++ args.py
@@ -1,10 +1,10 @@
 import argparse
 
 def get_args():
-    argparse = argparse.ArgumentParser()
-    argparse.add_argument("--use_reranker", type=bool, default=True, help="Use reranker or not")
-    argparse.add_argument("--retrieved_docs", type=int, default=5, help="Number of retrieved documents")
-    return  argparse.parse_args()
+    arg = argparse.ArgumentParser()
+    arg.add_argument("--use_reranker", type=bool, default=True, help="Use reranker or not")
+    arg.add_argument("--retrieved_docs", type=int, default=5, help="Number of retrieved documents")
+    return  arg.parse_args()
 
 if __name__ == "__main__":
     args = get_args()
