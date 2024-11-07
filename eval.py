d95fad7cc3f026f931af69ede0c32e0679008c7c
pack0shades <kumarpragay@gmail.com>
2024-11-08 00:33:46 +0530

    updated eval.py

--- eval.py
+++ eval.py
@@ -1,6 +1,6 @@
 import openai
 import pandas as pd
-
+import os
 
 
 def evaluate_answer(ground_truth, generated_answer):
@@ -19,15 +19,26 @@ def evaluate_answer(ground_truth, generated_answer):
     )
 
     output = response.choices[0].text.strip()
-    return int(output) if output in {"1", "0"} else 0  
+    return int(output) if output in {"1", "0"} else 0
 
-df = pd.read_csv("your_file.csv")
+def find_pdf(data_dir: str, filename: str)->str:
+    filename = filename + ".pdf"
+    for dirpath, part,dirnames, pdfs in os.walk(data_dir):
+        for file in pdfs:
+            if file == filename:
+                return os.path.join(dirpath, file)
+            else:
+                print(f"File {filename} not found in {dirpath}.AAAAAAAAAAAaaaaaaaaaaaaaAAAAAAAAAAAAAAAAA")
+                return None
 
-df["bool_val"] = df.apply(lambda row: evaluate_answer(row["ground_truth"], row["generated_answer"]), axis=1)
-df.to_csv("eval.csv", index=False)
-correct_sum = df["bool_val"].sum()
-num_columns = df.shape[1]
-print(100*(correct_sum/num_columns))
+if __name__ == "__main__":
+    df = pd.read_csv("your_file.csv")
+
+    df["bool_val"] = df.apply(lambda row: evaluate_answer(row["ground_truth"], row["generated_answer"]), axis=1)
+    df.to_csv("eval.csv", index=False)
+    correct_sum = df["bool_val"].sum()
+    num_columns = df.shape[1]
+    print(100*(correct_sum/num_columns))
 
 
 
