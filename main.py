72a1f9f08942b73bd83f628c0558cc630b294024
Laksh-Mendpara <b23cs1037@iitj.ac.in>
2024-11-09 14:13:15 +0530

    added args

--- main.py
+++ main.py
@@ -5,29 +5,62 @@ from chromadb_client import *
 from reranker import *
 from openai import OpenAI
 from typing import List
-from time import time
 from args import get_args
 from swarm_router import get_agents, multi_agent
-import time
+from agent import context_to_agent
+import argparse
 
-args = get_args()
+# Create the argument parser
+parser = argparse.ArgumentParser(description="Process a single query or multiple queries from a JSON file.")
+
+# Define arguments
+parser.add_argument("--use_reranker", default=False, action="store_true", help="Use the reranker to rerank the retrieved documents.")
+parser.add_argument("--retrieved_docs", type=int, default=5, help="Number of documents to retrieve.")
+parser.add_argument("--pipeline", default="nov9", help="Specify the pipeline to use. Options are: 'nov4', 'nov9'")
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Print parsed arguments to verify correct parsing
+print("Parsed arguments:", args)
 
 
 def get_context(collection, reranker, query: str) -> str:
     # Retrieve documents
-    retrieved_docs = return_final_retrieve_docs(query)  # list
+    retrieved_docs = retrieve_documents(collection, query)  # list
     if args.use_reranker:
         # Rerank documents
         reranked_docs = reranker.rerank_documents(query, retrieved_docs)
-        context = ""
-        for idx, doc in enumerate(reranked_docs):
-            context += f"Rank {idx + 1}: {doc}\n"
-        return context
     else:
-        return str(retrieved_docs)
+        reranked_docs = retrieved_docs
 
+    context = ""
+    for idx, doc in enumerate(reranked_docs):
+        context += f"Rank {idx + 1}: {doc}\n"
+    return context
 
-def generate_response_from_context(query: str, context) -> str:
+def generate_response_from_context(quer: str, context)-> str:
+    prompt = f'''Based on the following context, answer the user’s query accurately and concisely.
+
+    Context: {context}
+    User Query: {quer}
+    
+    Response:'''
+    client = OpenAI()
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "you are a useful assistant"},
+            {
+                "role": "user",
+                "content": f"{prompt}"
+            }
+        ]
+    )
+    return (completion.choices[0].message.content)
+
+def generate_response_from_multi_agent(query: str, context) -> str:
     agents, meta_agent, final_agent, router = get_agents()
 
     return multi_agent(agents, meta_agent, final_agent, router, query, context)
@@ -35,14 +68,22 @@ def generate_response_from_context(query: str, context) -> str:
 
 def pipeline(collection, reranker, query: str) -> str:
     context = get_context(collection, reranker, query)
-    res = generate_response_from_context(query, context)
-    return res
+    # print(f"ye rha context:::{context}")
+    if args.pipeline == "nov4":
+        final_context = context_to_agent(context)
+        res = generate_response_from_context(query, final_context)
+        return res
+    elif args.pipeline == "nov9":
+        res = generate_response_from_multi_agent(query, context)
+        return res
+    else:
+        print("use --pipeline argument to specify the pipeline, available options are: 'nov4', 'nov9'")
 
 
 def main():
     print(
         f"Using Reranker: {args.use_reranker}_____number of Retrieved Docs: {args.retrieved_docs}")
-    collection, collection_present = get_collection('collection-1731096205')
+    collection = get_collection()
     reranker_model = DocumentReranker()
 
     query = input("Enter your query: ")
